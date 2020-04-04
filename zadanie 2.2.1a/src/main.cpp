63e7365afd03f21801c5cd29d112b195b44ee6a8
Łukasz Drozd <lukasz.drozd1@student.wat.edu.pl>
2020-04-04 13:41:41 +0200

    Dodano kod i symulacje do zadania 2.2.1a

--- zadanie 2.2.1a/src/main.cpp
+++ zadanie 2.2.1a/src/main.cpp
@@ -2,10 +2,12 @@
 
 void setup()
 {
-
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop()
 {
-
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN, LOW);
 }
